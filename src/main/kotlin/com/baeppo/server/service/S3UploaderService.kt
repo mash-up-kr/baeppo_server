201d09a3e92e620f3a5df02e42b99ce8c98b2280
Seunghyeon-Kim <ssseungzz7@gmail.com>
2021-02-28 03:17:17 +0900

    add S3 uploader

---
+++ src/main/kotlin/com/baeppo/server/service/S3UploaderService.kt
@@ -0,0 +1,48 @@
+package com.baeppo.server.service
+
+import com.amazonaws.auth.AWSStaticCredentialsProvider
+import com.amazonaws.auth.BasicAWSCredentials
+import com.amazonaws.services.s3.AmazonS3
+import com.amazonaws.services.s3.AmazonS3ClientBuilder
+import com.amazonaws.services.s3.model.CannedAccessControlList
+import com.amazonaws.services.s3.model.PutObjectRequest
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.stereotype.Service
+import org.springframework.web.multipart.MultipartFile
+import javax.annotation.PostConstruct
+
+@Service
+class S3UploaderService() {
+    private lateinit var s3Client: AmazonS3
+
+    @Value("\${aws.cloud.aws.credentials.accessKey}")
+    private val accessKey: String? = null
+
+    @Value("\${aws.cloud.aws.credentials.secretKey}")
+    private val secretKey: String? = null
+
+    @Value("\${aws.cloud.aws.s3.bucket}")
+    private val bucket: String? = null
+
+    @Value("\${aws.cloud.aws.region.static}")
+    private val region: String? = null
+
+    @PostConstruct
+    fun setS3Client() {
+        val credentials = BasicAWSCredentials(this.accessKey, this.secretKey)
+
+        s3Client = AmazonS3ClientBuilder.standard()
+                .withCredentials(AWSStaticCredentialsProvider(credentials))
+                .withRegion(this.region)
+                .build()
+    }
+
+    fun upload(file: MultipartFile) : String {
+        val fileName = file.originalFilename
+
+        s3Client.putObject(PutObjectRequest(bucket, fileName, file.inputStream, null)
+                .withCannedAcl(CannedAccessControlList.PublicRead))
+        return s3Client.getUrl(bucket, fileName).toString()
+    }
+
+}
